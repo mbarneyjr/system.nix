7ef432da8aef50e11c0f21ac41d03fb28cb430ae
Michael Barney, Jr <mbarneyme@gmail.com>
2024-03-01 13:14:30 -0500

    feat(tsc): add ts-error-translator for better typescript errors in nvim

--- home/nvim/lua/barney/plugins/tsc.lua
+++ home/nvim/lua/barney/plugins/tsc.lua
@@ -1,8 +1,11 @@
 return {
   "dmmulroy/tsc.nvim",
+  dependencies = { "dmmulroy/ts-error-translator.nvim" },
   ft = { "typescript", "javascript" },
   config = function()
     local tsc = require("tsc")
+    local ts_error_translator = require("ts-error-translator")
     tsc.setup()
+    ts_error_translator.setup()
   end,
 }
