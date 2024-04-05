3042fae94cde8d4613d09f67c237fa813fc5ff5d
Michael Barney, Jr <mbarneyme@gmail.com>
2024-04-05 15:19:14 -0400

    push

--- linux/home.nix
+++ linux/home.nix
@@ -1,12 +1,56 @@
-{ username, config, pkgs, mbnvim, ... }:
+{ username, pkgs, unstable, mbnvim, ... }:
 
 {
   home.username = username;
   home.homeDirectory = "/home/${username}";
   home.stateVersion = "23.05";
-  home.packages = with pkgs; [
-    neofetch
-    neovim
+  home.packages = [
+    pkgs.neofetch
+    pkgs.neovim
+    pkgs.ripgrep
+    pkgs.jq
+    pkgs.fzf
+    pkgs.eza
+    pkgs.bat
+    pkgs.wget
+    pkgs.gnupg
+    pkgs.ack
+    unstable.awscli2
+    pkgs.fd
+    pkgs.ffmpeg
+    pkgs.fswatch
+    pkgs.gh
+    pkgs.graphviz
+    pkgs.imagemagick
+    pkgs.lolcat
+    pkgs.pandoc
+    pkgs.slides
+    pkgs.speedtest-cli
+    pkgs.alacritty
+    pkgs.docker
+    pkgs.aws-sam-cli
+    pkgs.pipx
+    pkgs.python311Packages.yq
+    pkgs.git-remote-codecommit
+    pkgs.ggshield
+    unstable.yarn-berry
+    pkgs.libcxxabi
+    pkgs.darwin.libiconv
+    pkgs.libcxxStdenv
+    pkgs.terraform
+    pkgs.swig
+    pkgs.nixpkgs-fmt
+    # nvim dependencies
+    pkgs.nodejs_20
+    pkgs.python311Full
+    pkgs.go
+    pkgs.cargo
+    pkgs.darwin.libiconv
+    pkgs.gopls
+    pkgs.cmake
+    pkgs.gnumake
+    # other
+    pkgs.swift
   ];
   home.file.neovim = {
     enable = true;
