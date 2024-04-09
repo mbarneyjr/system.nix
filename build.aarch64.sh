3c060190f6d36971560392c23d4d4a003ab1cd2d
Michael Barney, Jr <mbarneyme@gmail.com>
2024-04-09 14:57:08 -0400

    fix(build): fix experimental feature name

--- build.aarch64.sh
+++ build.aarch64.sh
@@ -2,5 +2,5 @@ SYSTEM=$(uname -s)
 if [ "$SYSTEM" = "Darwin" ]; then
   darwin-rebuild switch --flake ~/system.nix#aarch64
 else
-  nix --extra-experimental-features nix-command flakes run nixpkgs#home-manager -- switch --flake ~/system.nix#aarch64
+  nix --extra-experimental-features nix-command flake run nixpkgs#home-manager -- switch --flake ~/system.nix#aarch64
 fi
--- build.x86_64.sh
+++ build.x86_64.sh
@@ -2,5 +2,5 @@ SYSTEM=$(uname -s)
 if [ "$SYSTEM" = "Darwin" ]; then
   darwin-rebuild switch --flake ~/system.nix#x86_64
 else
-  nix --extra-experimental-features nix-command flakes run nixpkgs#home-manager -- switch --flake ~/system.nix#x86_64
+  nix --extra-experimental-features nix-command flake run nixpkgs#home-manager -- switch --flake ~/system.nix#x86_64
 fi
