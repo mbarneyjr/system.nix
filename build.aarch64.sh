c0f8c4c883ace5d7da40cb9fef03e0ad58193fe7
Michael Barney, Jr <mbarneyme@gmail.com>
2024-04-09 14:56:25 -0400

    fix(scripts): add extra experimental features flags to build scripts

--- build.aarch64.sh
+++ build.aarch64.sh
@@ -2,5 +2,5 @@ SYSTEM=$(uname -s)
 if [ "$SYSTEM" = "Darwin" ]; then
   darwin-rebuild switch --flake ~/system.nix#aarch64
 else
-  nix run nixpkgs#home-manager -- switch --flake ~/system.nix#aarch64
+  nix --extra-experimental-features nix-command flakes run nixpkgs#home-manager -- switch --flake ~/system.nix#aarch64
 fi
