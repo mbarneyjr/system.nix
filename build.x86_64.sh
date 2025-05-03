a7aadbacb23db40a22a7d6661d85b07107ccb4d7
Michael Barney, Jr <mbarneyme@gmail.com>
2025-05-03 19:55:42 -0400

    fix(build.x86_64.sh): run nix-darwin from nixpkgs

--- build.x86_64.sh
+++ build.x86_64.sh
@@ -1,6 +1,6 @@
 SYSTEM=$(uname -s)
 if [ "$SYSTEM" = "Darwin" ]; then
-  darwin-rebuild switch --flake ~/system.nix#x86_64
+  nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin/nix-darwin-24.11#darwin-rebuild -- switch --flake ~/system.nix#x86_64
 else
   nix run --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#home-manager -- switch --flake ~/system.nix#x86_64
 fi
