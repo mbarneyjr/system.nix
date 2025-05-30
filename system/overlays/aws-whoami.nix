8840044625d4f16437afcf0b3f3a00dd6a01d1e5
Michael Barney, Jr <mbarneyme@gmail.com>
2025-05-30 08:56:58 -0400

    fix(overlays): fix erroring aws-whoami overlay

--- system/overlays/aws-whoami.nix
+++ system/overlays/aws-whoami.nix
@@ -1,8 +1,8 @@
 final: prev: {
-  aws-whoami = prev.python3Packages.buildPythonApplication rec {
+  aws-whoami = prev.python3Packages.buildPythonPackage rec {
     pname = "aws-whoami";
     version = "1.2.0";
-    pyproject = true;
+    format = "setuptools";
 
     src = prev.fetchPypi {
       pname = "aws-whoami";
@@ -10,33 +10,17 @@ final: prev: {
       hash = "sha256-a53jpM9sPFqhGKFzyYfSaRYHAnO2d+98swEGQD+GLRg=";
     };
 
-    nativeBuildInputs = [
-      prev.poetry
-    ];
-    build-system = [
-      prev.python3Packages.poetry-core
-      prev.python3Packages.findpython
-      prev.python3Packages.pbs-installer
-      prev.python3Packages.pbs-installer.optional-dependencies.download
-      prev.python3Packages.pbs-installer.optional-dependencies.install
-      prev.python3Packages.tomlkit
-      prev.python3Packages.xattr
-      prev.python3Packages.cleo
-      prev.python3Packages.shellingham
-      prev.python3Packages.fastjsonschema
-      prev.python3Packages.dulwich
-      prev.python3Packages.virtualenv
-      prev.python3Packages.requests-toolbelt
-      prev.python3Packages.pkginfo
-      prev.python3Packages.trove-classifiers
-      prev.python3Packages.keyring
-      prev.python3Packages.requests
-      prev.python3Packages.cachecontrol
-      prev.python3Packages.platformdirs
-    ];
-
-    dependencies = with prev.python3Packages; [
+    propagatedBuildInputs = with prev.python3Packages; [
       boto3
     ];
+
+    # Allow using boto3 directly instead of checking for imports
+    pythonImportsCheck = [];
+
+    meta = with prev.lib; {
+      description = "CLI tool to print out the current AWS identity";
+      homepage = "https://github.com/benkehoe/aws-whoami";
+      license = licenses.mit;
+    };
   };
 }
