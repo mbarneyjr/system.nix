4116fff50ceda24860be1c663ebdd81c4226e4d7
Michael Barney, Jr <mbarneyme@gmail.com>
2025-11-03 09:09:05 -0500

    feat(aws-sam-cli): override aws_lambda_builders dependency

--- system/overlays/aws-sam-cli.nix
+++ system/overlays/aws-sam-cli.nix
@@ -2,6 +2,7 @@ final: prev: {
   aws-sam-cli = prev.aws-sam-cli.overrideAttrs (old: {
     patchPhase = ''
       substituteInPlace requirements/base.txt --replace "click==8.1.8" "click"
+      substituteInPlace requirements/base.txt --replace "aws_lambda_builders==1.57.0" "aws_lambda_builders"
     '';
     doCheck = false;
     disabledTests = old.disabledTests or [ ] ++ [
