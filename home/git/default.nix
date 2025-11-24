22229ee96f7f5275584acf4772563212fd36f756
Michael Barney, Jr <mbarneyme@gmail.com>
2025-11-24 09:58:59 -0500

    fix(git): update git config to new module spec

--- home/git/default.nix
+++ home/git/default.nix
@@ -23,87 +23,90 @@
   };
   programs.git = {
     enable = true;
-    package = pkgs.git;
+  };
+
+  programs.git.settings = {
+    user.email = "mbarneyme@gmail.com";
+    user.name = "Michael Barney, Jr";
+    user.signingkey = "0383630977773772";
+    commit.gpgsign = true;
     aliases = {
       ls = "log --graph --abbrev-commit --color --pretty=format:'%C(yellow)%h%Creset -%Cred%d%Creset %s %Cgreen(%cr) %C(blue)[%an]%Creset'";
     };
-    ignores = [
-      ".DS_Store"
-      ".nlsp-settings"
-      ".cache_ggshield"
-      "orb.db"
-      ".nvim.lua"
-      ".envrc"
-      ".direnv/"
-      "*.barneylocal"
-      "*.barneylocal.*"
-    ];
-    userEmail = "mbarneyme@gmail.com";
-    userName = "Michael Barney, Jr";
-    includes = [
-      {
-        condition = "gitdir:~/dev/work/";
-        contents = {
-          user.email = "mbarney@trek10.com";
-          user.name = "Michael Barney";
-        };
-      }
-    ];
     difftastic = {
       enable = true;
     };
-    attributes = [
-      "* merge=mergiraf"
-    ];
-    extraConfig = {
-      "credential \"https://github.com\"" = {
-        helper = "!${pkgs.gh}/bin/gh auth git-credential";
-      };
-      "credential \"https://gist.github.com\"" = {
-        helper = "!${pkgs.gh}/bin/gh auth git-credential";
-      };
-      user.signingkey = "0383630977773772";
-      commit.gpgsign = true;
-      core = {
-        editor = "nvim";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      status = {
-        showUntrackedFiles = "all";
-      };
-      merge = {
-        conflictstyle = "diff3";
-        tool = "nvimdiff";
-      };
-      merge.mergiraf = {
-        name = "mergiraf";
-        driver = "mergiraf merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
-      };
-      diff = {
-        tool = "nvimdiff";
-      };
-      mergetool = {
-        keepBackup = false;
-      };
-      pull = {
-        rebase = true;
-      };
-      fetch = {
-        prune = true;
-        pruneTabs = true;
-        recurseSubmodules = true;
-      };
-      push = {
-        autoSetupRemote = true;
-      };
-      rerere = {
-        enabled = true;
-      };
-      "protocol \"file\"" = {
-        allow = "always";
-      };
+    "credential \"https://github.com\"" = {
+      helper = "!${pkgs.gh}/bin/gh auth git-credential";
+    };
+    "credential \"https://gist.github.com\"" = {
+      helper = "!${pkgs.gh}/bin/gh auth git-credential";
+    };
+    core = {
+      editor = "nvim";
+    };
+    init = {
+      defaultBranch = "main";
+    };
+    status = {
+      showUntrackedFiles = "all";
+    };
+    merge = {
+      conflictstyle = "diff3";
+      tool = "nvimdiff";
+    };
+    merge.mergiraf = {
+      name = "mergiraf";
+      driver = "mergiraf merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
+    };
+    diff = {
+      tool = "nvimdiff";
+    };
+    mergetool = {
+      keepBackup = false;
+    };
+    pull = {
+      rebase = true;
+    };
+    fetch = {
+      prune = true;
+      pruneTabs = true;
+      recurseSubmodules = true;
+    };
+    push = {
+      autoSetupRemote = true;
+    };
+    rerere = {
+      enabled = true;
+    };
+    "protocol \"file\"" = {
+      allow = "always";
     };
   };
+
+  programs.git.ignores = [
+    ".DS_Store"
+    ".nlsp-settings"
+    ".cache_ggshield"
+    "orb.db"
+    ".nvim.lua"
+    ".envrc"
+    ".direnv"
+    "*.barneylocal"
+    "*.barneylocal.*"
+  ];
+
+  programs.git.includes = [
+    {
+      condition = "gitdir:~/dev/work/";
+      contents = {
+        user.email = "mbarney@trek10.com";
+        user.name = "Michael Barney";
+      };
+    }
+  ];
+
+  programs.git.attributes = [
+    "* merge=mergiraf"
+  ];
 }
