3bf2c5694e6327cfe2283558bca12a2517d53715
Michael Barney, Jr <mbarneyme@gmail.com>
2026-03-09 10:06:06 -0400

    feat(awsuse): add awsuse-sso script for sso credential retrieval

--- home/awsuse/default.nix
+++ home/awsuse/default.nix
@@ -3,4 +3,10 @@
     _awsuse_console_services_file=${./services.txt}
     ${builtins.readFile ./shell.sh}
   '';
+  home.file.awsuse-sso = {
+    enable = true;
+    executable = true;
+    source = ./awsuse-sso;
+    target = ".local/bin/awsuse-sso";
+  };
 }
