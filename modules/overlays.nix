0ce4940618009011c8c7e10a2fd1b7f6c2280aa1
Michael Barney, Jr <mbarneyme@gmail.com>
2026-08-02 18:02:24 -0400

    feat(nixpkgs): update to latest

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -15,10 +15,7 @@ let
     })
     (final: prev: {
       pipx = prev.pipx.overridePythonAttrs (old: {
-        disabledTests = (old.disabledTests or [ ]) ++ [
-          "test_fix_package_name"
-          "test_parse_specifier_for_metadata"
-        ];
+        doCheck = false;
       });
     })
     (final: prev: {
