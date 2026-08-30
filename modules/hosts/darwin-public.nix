fcd70594d17851ec3a87709b1341120f38306821
Michael Barney, Jr <mbarneyme@gmail.com>
2026-08-30 12:55:49 -0400

    refactor(hosts): remove darwin configurations abstraction module

--- modules/hosts/darwin-public.nix
+++ modules/hosts/darwin-public.nix
@@ -3,43 +3,42 @@ let
   inherit (config.flake.modules) darwin homeManager;
 in
 {
-  configurations.darwin = {
-    aarch64-public.module = {
-      imports = [
-        darwin.overlays
-        darwin.nix-settings
-        darwin.darwin-settings
-        darwin.homebrew
-        darwin.aerospace
-        darwin.sketchybar
-        darwin.fonts
-        darwin.user
-        darwin.agents
+  flake.darwinConfigurations.aarch64-public = inputs.nix-darwin.lib.darwinSystem {
+    modules = [
+      darwin.overlays
+      darwin.nix-settings
+      darwin.darwin-settings
+      darwin.homebrew
+      darwin.aerospace
+      darwin.sketchybar
+      darwin.fonts
+      darwin.user
+      darwin.agents
 
-        inputs.home-manager.darwinModules.home-manager
-        {
-          services.sketchybar.enable = false;
-          proprietary.berkeleyMono.enable = false;
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${config.user.name} = {
-            imports = [
-              homeManager.user
-              homeManager.packages
-              homeManager.zsh
-              homeManager.direnv
-              homeManager.git
-              homeManager.tmux
-              homeManager.ghostty
-              homeManager.awsume
-              homeManager.awsuse
-              homeManager.agents
-              homeManager.ssh
-            ];
-          };
-        }
-      ];
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+      inputs.home-manager.darwinModules.home-manager
+      {
+        services.sketchybar.enable = false;
+        proprietary.berkeleyMono.enable = false;
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${config.user.name} = {
+          imports = [
+            homeManager.user
+            homeManager.packages
+            homeManager.zsh
+            homeManager.direnv
+            homeManager.git
+            homeManager.tmux
+            homeManager.ghostty
+            homeManager.awsume
+            homeManager.awsuse
+            homeManager.agents
+            homeManager.ssh
+          ];
+        };
+      }
+
+      { nixpkgs.hostPlatform = "aarch64-darwin"; }
+    ];
   };
 }
--- modules/hosts/darwin.nix
+++ modules/hosts/darwin.nix
@@ -3,46 +3,45 @@ let
   inherit (config.flake.modules) darwin homeManager;
 in
 {
-  configurations.darwin = {
-    aarch64.module = {
-      imports = [
-        darwin.overlays
-        darwin.nix-settings
-        darwin.darwin-settings
-        darwin.homebrew
-        darwin.aerospace
-        darwin.sketchybar
-        darwin.fonts
-        darwin.user
-        darwin.agents
+  flake.darwinConfigurations.aarch64 = inputs.nix-darwin.lib.darwinSystem {
+    modules = [
+      darwin.overlays
+      darwin.nix-settings
+      darwin.darwin-settings
+      darwin.homebrew
+      darwin.aerospace
+      darwin.sketchybar
+      darwin.fonts
+      darwin.user
+      darwin.agents
 
-        inputs.home-manager.darwinModules.home-manager
-        {
-          services.sketchybar.enable = false;
-          proprietary.berkeleyMono.enable = true;
-          proprietary.berkeleyMono.rev = "cefdbcc46656894441902c8e64712e83420a9fa4";
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${config.user.name} = {
-            imports = [
-              homeManager.user
-              homeManager.packages
-              homeManager.zsh
-              homeManager.direnv
-              homeManager.git
-              homeManager.tmux
-              homeManager.ghostty
-              homeManager.awsume
-              homeManager.awsuse
-              homeManager.agents
-              homeManager.ssh
-              homeManager.npm
-              homeManager.tampermonkey
-            ];
-          };
-        }
-      ];
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+      inputs.home-manager.darwinModules.home-manager
+      {
+        services.sketchybar.enable = false;
+        proprietary.berkeleyMono.enable = true;
+        proprietary.berkeleyMono.rev = "cefdbcc46656894441902c8e64712e83420a9fa4";
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${config.user.name} = {
+          imports = [
+            homeManager.user
+            homeManager.packages
+            homeManager.zsh
+            homeManager.direnv
+            homeManager.git
+            homeManager.tmux
+            homeManager.ghostty
+            homeManager.awsume
+            homeManager.awsuse
+            homeManager.agents
+            homeManager.ssh
+            homeManager.npm
+            homeManager.tampermonkey
+          ];
+        };
+      }
+
+      { nixpkgs.hostPlatform = "aarch64-darwin"; }
+    ];
   };
 }
